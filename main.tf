d98b88097d6a647b3f03beece67edb7c8c427b85
joshchontw <chojaeo@sheridancollege.ca>
2022-03-27 16:07:16 -0400

    small fix

--- main.tf
+++ main.tf
@@ -1,22 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-
-  backend "s3" {
-    bucket = "lab-03-terraform"
-    key    = "part3b_state"
-    region = "us-east-1"
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_vpc" "test_vpc" {
     cidr_block = "10.0.0.0/16"
     enable_dns_support = true
