0ca645f31a6c77bccd41de8fabc8290a0d080225
Josh Cho <81763406+joshchontw@users.noreply.github.com>
2022-04-04 12:41:05 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,4 +2,8 @@ resource "aws_vpc" "test_vpc" {
     cidr_block = "10.0.0.0/16"
     enable_dns_support = true
     enable_dns_hostnames = true
+    
+    tags = {
+    Name = "josh's-demo-vpc"
+  }
 }
